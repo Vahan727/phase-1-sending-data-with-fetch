1c7e2663e6ceecdb2247022e4d3e34bc5ad87cd9
Vahan Nadjarian <vahan@nadjarian.com>
2023-04-14 16:12:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(userName, userEmail) {
+    const userData = {
+        name: `${userName}`,
+        email: `${userEmail}`,
+      };
+      const data = {
+        method: 'POST',
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+      }
+    return fetch('http://localhost:3000/users', data)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(object){
+        console.log(object)
+      })
+      .catch(function(error) {
+        alert('Error')
+        console.log(error)
+      })
+    }
+    submitData('John', 'john@email.com')
